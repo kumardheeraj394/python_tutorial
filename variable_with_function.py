5270a2f77123ba406a2b84d77049eb6a58b0dd84
kumardheeraj394 <60124429+kumardheeraj394@users.noreply.github.com>
2025-04-29 14:34:16 +0530

    Update variable_with_function.py

--- variable_with_function.py
+++ variable_with_function.py
@@ -5,4 +5,20 @@ def addition():
     add = num1 + num2
     print(add)
 
-addition()  # Correct way to call the function
+def sub():
+    sub = num1 - num2
+    print(sub)  
+
+def mul():
+    mul = num1 * num2
+    print(mul) 
+
+def div():
+    div = num1 / num2
+    print(div)         
+
+#addition()  # Correct way to call the function
+#sub()
+#mul()
+div()
+
