b6a67bb32c1188511276173468f549dffececea1
kumardheeraj394 <60124429+kumardheeraj394@users.noreply.github.com>
2025-04-29 14:58:01 +0530

    Create import.py

---
+++ import.py
@@ -0,0 +1,12 @@
+import function_return as calculator
+
+result1 = calculator.addition(10, 5)
+result2 = calculator.sub(10, 5)
+result3 = calculator.mul(10, 5)
+result4 = calculator.div(10, 5)
+
+# Use functions from the module
+print("Addition:", result1)
+print("Subtraction:", result2)
+print("Multiplication:", result3)
+print("Multiplication:", result4)
