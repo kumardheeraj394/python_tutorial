816a0ef5a1d1e4c0f934b4249e41dc5b247d72d6
kumardheeraj394 <60124429+kumardheeraj394@users.noreply.github.com>
2025-04-29 18:27:09 +0530

    Create 1-user_input_from_cmd_argu.py

---
+++ Day-5/1-user_input_from_cmd_argu.py
@@ -0,0 +1,21 @@
+import sys
+
+def addition(num1, num2):
+    add = num1 + num2
+    return add
+
+def sub(num1, num2):
+    s = num1 - num2
+    return s
+
+def mul(num1, num2):
+    m = num1 * num2
+    return m
+
+def div(num1, num2):
+    d = num1 / num2
+    return d
+
+num1 = sys.argv[1]:
+operation = sys.argv[2]:
+num2 = sys.argv[3]:
