0a022906706104e50e8963148fe6160f65cc8275
kumardheeraj394 <60124429+kumardheeraj394@users.noreply.github.com>
2025-04-29 14:21:54 +0530

    Create function_return.py

---
+++ function_return.py
@@ -0,0 +1,16 @@
+def addition(num1, num2):
+    add = num1 + num2
+    return add
+
+def sub(num1, num2):
+    s = num1 - num2
+    return s
+
+def mul(num1, num2):
+    m = num1 * num2
+    return m
+
+# Calling all functions and printing their return values
+print("Addition:", addition(6, 10))
+print("Subtraction:", sub(10, 5))
+print("Multiplication:", mul(5, 10))
