a8ea24586776a9ed18a85ffdf482b4ea3ed48bd5
kumardheeraj394 <60124429+kumardheeraj394@users.noreply.github.com>
2025-04-30 10:09:01 +0530

    Update 1-user_input_from_cmd_argu.py

--- Day-5/1-user_input_from_cmd_argu.py
+++ Day-5/1-user_input_from_cmd_argu.py
@@ -16,6 +16,10 @@ def div(num1, num2):
     d = num1 / num2
     return d
 
-num1 = sys.argv[1]:
+num1 = int(sys.argv[1]):
 operation = sys.argv[2]:
-num2 = sys.argv[3]:
+num2 = int(sys.argv[3]):
+
+if operation == "add":
+    output = add(num1 num2)
+    print(output)
