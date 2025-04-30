0b103954a3b458e4cf68440b8d6220c22e82ff08
kumardheeraj394 <60124429+kumardheeraj394@users.noreply.github.com>
2025-04-30 14:21:11 +0530

    Create Assignment4.py

---
+++ Day-6/Assignment4.py
@@ -0,0 +1,11 @@
+#Create a variable total and initialize it to 10.
+#Use assignment operators (+=, -=, *=, /=) to update the value of total.
+#Print the final value of total.
+total = 10
+
+total += 5
+total -= 3
+total *= 2
+total /= 4
+
+print("Final total:", total)
