f68f740b1b5adc1e0af8886b23ae35e69ca92804
kumardheeraj394 <60124429+kumardheeraj394@users.noreply.github.com>
2025-04-30 14:31:55 +0530

    Update Assignment4.py

--- Day-6/Assignment4.py
+++ Day-6/Assignment4.py
@@ -3,9 +3,12 @@
 #Print the final value of total.
 total = 10
 
-total += 5
-total -= 3
-total *= 2
-total /= 4
+total += 5 # 15 this will use as total for subtraction
+print("10+5:", total)
+total -= 3 #15-3=12 total for multification
+print("15-3:", total)
+total *= 2 # 12*2=24 total for division
+print("12*2:", total)
+total /= 4 # 24/4=6
 
-print("Final total:", total)
+print("Final total 24/4:", total)
