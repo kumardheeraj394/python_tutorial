06591c279e7f4c01f0dc5d941d02ea75544f6835
kumardheeraj394 <60124429+kumardheeraj394@users.noreply.github.com>
2025-04-30 13:09:40 +0530

    Create assignment3.py

---
+++ Day-6/assignment3.py
@@ -0,0 +1,16 @@
+#Create two boolean variables, x and y.
+#Use logical operators (and, or, not) to perform various logical operations on x and y.
+#Print the results.
+a = 2
+b = 3
+x = a is b
+
+c = 4
+d = 5
+y = c is d
+
+# Use logical operators (and, or, not) to perform logical operations
+print("x and y:", x and y)     # True and True -> True
+print("x or y:", x or y)       # True or True -> True
+print("not x:", not x)         # not True -> False
+print("not y:", not y)         # not True -> False
