c4dc6916791372040247b567a236594692adc703
kumardheeraj394 <60124429+kumardheeraj394@users.noreply.github.com>
2025-05-01 10:11:24 +0530

    Update and rename Conditional.py to conditional.py

---
+++ Day-7/conditional.py
@@ -0,0 +1,11 @@
+import sys            # use for command line argument
+
+type = sys.argv[1]    #define the command line argument
+if type == "a1":
+  print("creating a1")
+elif type == "a2":
+  print("creating a2")
+elif type == "a3":
+  print("creating a3")
+else:
+  print("not valid")
