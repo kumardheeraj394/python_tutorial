b83f6aaf0cf2817a4298ef3b14314608187cc724
kumardheeraj394 <60124429+kumardheeraj394@users.noreply.github.com>
2025-05-05 17:42:07 +0530

    Update list.py

--- Day-8/list.py
+++ Day-8/list.py
@@ -9,3 +9,7 @@ list =["a", "b", "c", "d"]
 list.remove("c")  # Use parentheses, not square brackets, to add element
 print(list)
 print(type(list))
+
+#Index
+list =["a", "b", "c", "d", "f"]
+print(list[1]) #for index use []
