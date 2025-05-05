de6aed71696054cfa44bac72055d87075977e1fd
kumardheeraj394 <60124429+kumardheeraj394@users.noreply.github.com>
2025-05-05 17:27:59 +0530

    Update list.py

--- Day-8/list.py
+++ Day-8/list.py
@@ -1,4 +1,11 @@
+#Program to append
 list =["a", "b", "c", "d"]
 list.append("e")  # Use parentheses, not square brackets, to add element
+print(list)
+print(type(list))
+
+#Program to remove
+list =["a", "b", "c", "d"]
+list.remove("c")  # Use parentheses, not square brackets, to add element
 print(list)
 print(type(list))
