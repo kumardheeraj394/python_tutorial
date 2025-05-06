21381385669a9917751b310e8199a90dad3ef004
kumardheeraj394 <60124429+kumardheeraj394@users.noreply.github.com>
2025-05-06 14:37:49 +0530

    Update for_loops.py

--- Day-9/for_loops.py
+++ Day-9/for_loops.py
@@ -10,3 +10,19 @@ color = ["red", "green", "yellow"] #color is list
 for x in color:  # use parentheses with range
     print(x)  
     #print()  
+
+# break
+print("--- Use of break ---")
+number = [1, 2, 3, 4, 5, 6]  # number is a list
+for x in number:
+    print(x)
+    if x == 4:
+        break  # exit the loop when x is 4 
+
+# continue
+print("--- Use of continue ---")
+number = [1, 2, 3, 4, 5, 6]
+for y in number:
+    if y == 4:
+        continue  # skip the print when y == 4
+    print(y)
