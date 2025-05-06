5760dce95f3b2ea25c5ddd6b2c460f7937f32b66
kumardheeraj394 <60124429+kumardheeraj394@users.noreply.github.com>
2025-05-06 12:42:59 +0530

    Update loops.py

--- Day-9/loops.py
+++ Day-9/loops.py
@@ -1,4 +1,12 @@
 # 'for', 'in', and 'range' are Python keywords, and 'i' is a variable
+print("---use range----")
 for i in range(10):  # use parentheses with range
-    print("dheeraj")
-    print("kumar")
+    print(i)
+   # print()
+
+# 'for', 'in', and 'color' are Python keywords, and 'x' is a variable
+print("---use list----")
+color = ["red", "green", "yellow"] #color is list
+for x in color:  # use parentheses with range
+    print(x)  
+    #print()  
