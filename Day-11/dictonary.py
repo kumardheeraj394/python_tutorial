88f5251c8ea3ce690114894304f6bd1764db39b9
kumardheeraj394 <60124429+kumardheeraj394@users.noreply.github.com>
2025-05-07 14:23:55 +0530

    Create dictonary.py

---
+++ Day-11/dictonary.py
@@ -0,0 +1,18 @@
+students = [
+  {
+    "name": = "abc",
+    "roll_no": = "1",
+    "class": = :"10"
+   }
+    {
+    "name": = "abcd",
+    "roll_no": = "2",
+    "class": = :"10"
+   }
+    {
+    "name": = "abcde",
+    "roll_no": = "3",
+    "class": = :"10"
+   }
+]
+print(students)
