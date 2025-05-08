6cd11d8c40618598fd44daa36fbf15c14119eb4c
kumardheeraj394 <60124429+kumardheeraj394@users.noreply.github.com>
2025-05-08 16:07:12 +0530

    Update dictonary_for_git.py

--- Day-11/dictonary_for_git.py
+++ Day-11/dictonary_for_git.py
@@ -1,1 +1,11 @@
+import requests
 
+repo = "kubernetes/kubernetes"
+url = f'https://api.github.com/repos/{repo}/pulls'
+response = requests.get(url)
+raw_data = response.json()
+#print(raw_data)
+
+for i in range(len(raw_data)):
+    print(type(raw_data))
+    
